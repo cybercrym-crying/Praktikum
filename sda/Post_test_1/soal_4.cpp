74036ecfcb8c32329002f06a2f2546e8fc7abd28
bero <crymbero@gmail.com>
2026-02-27 16:40:26 +0800

    move file , and create new folder

---
+++ sda/Post_test_1/soal_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void SWAP(int *a, int *b) {
+  int temp;
+  cout << "Var a : " << *a << " \t| " << a << endl;
+  cout << "Var b : " << *b << " \t| " << b << endl;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+  cout << "Var a : " << *a << " \t| " << a << endl;
+  cout << "Var b : " << *b << " \t| " << b << endl;
+};
+int main() {
+  int a = 5, b = 2;
+  SWAP(&a, &b);
+  return 0;
+}
